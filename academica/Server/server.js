f03d62b2d7ca46430de8c8f22d5abb873afb9473
valen500 <brianalvarado078@gmail.com>
2026-06-06 22:21:12 -0600

    mongo para insercion de datos

--- academica/Server/server.js
+++ academica/Server/server.js
@@ -1,11 +1,25 @@
 var http = require('http').Server(),
-    io = require('socket.io')(http);
+    io = require('socket.io')(http),
+    {MongoClient, ObjectId} = require('mongodb'),
+    url = 'mongodb://localhost:27017',
+    client = new MongoClient(url),
+    dbname = 'chats_ugb';
+
+const crypto = require('crypto');
+global.crypto = crypto.webcrypto;
+
+async function conectarMongo(){
+    await client.connect();
+    return client.db(dbname);
+}
 
 io.on('connect', (socket) => {
     console.log('Un usuario se ha conectado');
 
-    socket.on('mensajeRecibido', (data) => {
-        console.log(data);
+    socket.on('mensajeRecibido', async (data) => {
+        let db = await conectarMongo(),
+            collection = db.collection('chats'),
+            result = collection.insertOne({user:data.titulo, mensaje:data.mensaje, fecha:new Date()});
         io.emit('mensajeEnviar', data);
     })
 });
